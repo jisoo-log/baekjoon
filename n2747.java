846c5850dec78b08bd7364f5ae17926559843f28
jisoo lee <jisoolee.dev@gmail.com>
2019-12-18 19:45:54 +0900

    SOLVE : 2747
    DP

---
+++ n2747.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class n2747{
+	public static int[] list;
+
+	public static int dp(int n){
+		if(n==0) list[0]=0;
+		else if(n==1) list[1]=1;
+		else if(n==2) list[2]=1;
+		else if(list[n]==0)  {
+			list[n] = dp(n-2)+dp(n-1);
+		}
+		return list[n];
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		list = new int[n+1];
+		System.out.print(dp(n));
+	} 
+
+}
